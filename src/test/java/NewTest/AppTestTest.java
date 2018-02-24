8e89261a7ed1547f9eb941c20a5edfe6c8225133
Nurali Abliz <Nur@Nuralis-MacBook-Pro.local>
2018-02-24 16:10:51 -0500

    My second Maven Commit

---
+++ src/test/java/NewTest/AppTestTest.java
@@ -0,0 +1,38 @@
+package NewTest;
+
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+
+public class AppTestTest {
+	
+	WebDriver driver;
+  @Test
+  public void Maven() {
+	  WebElement search_box=driver.findElement(By.name("q"));
+	  search_box.sendKeys("porsche"+Keys.ENTER);
+	  AssertJUnit.assertTrue((driver.getTitle().contains("porsche")));
+  }
+  @BeforeTest
+  public void beforeTest() {
+	  WebDriverManager.chromedriver().setup();
+	  driver=new ChromeDriver();
+	  driver.navigate().to("https://www.google.com/");
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
